4918b7139e57b413e5a8c3d660167281be31cce9
Spencer Maxfield <smaxfield@duosecurity.com>
2018-11-18 16:36:48 -0500

    Converted page to use sprites; finalized gulp sprite automation

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var svgSprite = require('gulp-svg-sprite');
+var rename = require('gulp-rename');
+var del = require('del');
 
-gulp.task('createSprite', function() {
+gulp.task('beginClean', function() {
+	return del(['./app/temp/sprite', './app/assets/images/sprites']);
+})
+
+gulp.task('createSprite', ['beginClean'], function() {
 	return gulp.src('./app/assets/images/icons/**/*.svg')
 		.pipe(svgSprite({
 			mode: {
 				css: {
+					sprite: 'sprite.svg',
 					render: {
 						css: {
 							template: './gulp/templates/sprite.css'
@@ -16,3 +23,20 @@ gulp.task('createSprite', function() {
 		}))
 		.pipe(gulp.dest('./app/temp/sprite'));
 });
+
+gulp.task('copySpriteGraphic', ['createSprite'], function() {
+	return gulp.src('./app/temp/sprite/css/**/*.svg')
+		.pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+gulp.task('copySpriteCss', ['createSprite'], function() {
+	return gulp.src('./app/temp/sprite/css/*.css')
+		.pipe(rename(('_sprite.css')))
+		.pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('endClean', ['copySpriteCss', 'copySpriteGraphic'],function() {
+	return del('./app/temp/sprite')
+});
+
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCss', 'endClean']);
