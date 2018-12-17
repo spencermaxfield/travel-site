0c1833b18e3f6492011c20ab78fc4adcce89d844
Spencer Maxfield <smaxfield@duosecurity.com>
2018-12-16 19:57:31 -0500

    Used modernizr to supply png images when necessary

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var svgSprite = require('gulp-svg-sprite');
 var rename = require('gulp-rename');
 var del = require('del');
+var svg2png = require('gulp-svg2png');
 
 gulp.task('beginClean', function() {
 	return del(['./app/temp/sprite', './app/assets/images/sprites']);
@@ -12,6 +13,13 @@ gulp.task('createSprite', ['beginClean'], function() {
 		.pipe(svgSprite({
 			mode: {
 				css: {
+					variables: {
+						replaceSvgWithPng: function() {
+							return function(sprite, render) {
+								return render(sprite).split('.svg').join('.png');
+							}
+						}
+					},
 					sprite: 'sprite.svg',
 					render: {
 						css: {
@@ -24,8 +32,14 @@ gulp.task('createSprite', ['beginClean'], function() {
 		.pipe(gulp.dest('./app/temp/sprite'));
 });
 
-gulp.task('copySpriteGraphic', ['createSprite'], function() {
-	return gulp.src('./app/temp/sprite/css/**/*.svg')
+gulp.task('createPngCopy', ['createSprite'], function() {
+	return gulp.src('./app/temp/sprite/css/*.svg')
+		.pipe(svg2png())
+		.pipe(gulp.dest('./app/temp/sprite/css'))
+});
+
+gulp.task('copySpriteGraphic', ['createPngCopy'], function() {
+	return gulp.src('./app/temp/sprite/css/**/*.{svg,png}')
 		.pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
@@ -39,4 +53,4 @@ gulp.task('endClean', ['copySpriteCss', 'copySpriteGraphic'],function() {
 	return del('./app/temp/sprite')
 });
 
-gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCss', 'endClean']);
+gulp.task('icons', ['beginClean', 'createSprite', 'createPngCopy', 'copySpriteGraphic', 'copySpriteCss', 'endClean']);
